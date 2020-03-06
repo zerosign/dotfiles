91716764b5c04aac2e7b407fcadfd7917b0ad59a
zerosign <r1nlx0@gmail.com>
2020-03-06 21:54:17 +0700

    update emacs init.el

--- emacs/init.el
+++ emacs/init.el
@@ -2,26 +2,23 @@
 ;;; Commentary: this is zerosign init.el
 ;;; Code:
 
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;; (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; (when (file-exists-p custom-file)
+  ;; (load custom-file))
 
 ;; TODO : evaluate emacs-tree-sitter
 ;; (add-to-list 'load-path "/home/zerosign/Repositories/rust/emacs-tree-sitter")
 
-;; (add-to-list 'default-frame-alist '(font . "Source Code Pro-10"))
-;; (set-face-attribute 'default t :font "Source Code Pro-10")
-;; (set-face-attribute 'default nil :font "Source Code Pro-10")
-;; (set-frame-font "Source Code Pro-10" nil t)
-
-(add-to-list 'default-frame-alist '(font . "Fira Code-9:Regular"))
-(set-face-attribute 'default t :font "Fira Code-9:Regular")
-(set-face-attribute 'default nil :font "Fira Code-9:Regular")
-(set-frame-font "Fira Code-9:Regular" nil t)
+(add-to-list 'default-frame-alist '(font . "Cascadia Mono-9:Regular"))
+(set-face-attribute 'default t :font "Cascadia Mono-9:Regular")
+(set-face-attribute 'default nil :font "Cascadia Mono-9:Regular")
+(set-frame-font "Cascadia Mono-9:Regular" nil t)
 
 (require 'package)
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")))
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org"   . "https://orgmode.org/elpa")
+                         ("ublt" . "https://elpa.ubolonton.org/packages/")))
 
 (package-initialize)
 
@@ -58,9 +55,9 @@
 
 ;; begin envs setup
 
-;; nvm env variable setups
+(defconst repository-dir (expand-file-name "Repositories" (getenv "HOME")))
+(defconst blog-dir (expand-file-name "blog/zerosign.github.io" repository-dir))
 (defconst twitter-dir (expand-file-name "twitter" (expand-file-name ".config" (concat (getenv "HOME")))))
-
 (defconst nvm-dir (concat (getenv "HOME") "/.nvm"))
 (defconst caches-dir (expand-file-name "caches" user-emacs-directory))
 
@@ -88,10 +85,12 @@
 (setq extra-paths
       (list (concat (getenv "HOME") "/go/bin")
             (concat (getenv "HOME") "/.cargo/bin")
+            (concat (getenv "HOME") "/.opam/4.09.0+fp+flambda/bin")
             (concat (getenv "HOME") "/.local/bin")
             (nvm-bin-path nvm-dir)))
 
 ;; (add-to-list 'load-path "/opt/mu/share/emacs/site-lisp/mu4e")
+
 
 (defun set-external-paths (externals)
   (let ((paths (append (delete-dups (split-string (getenv "PATH") ":")) externals)))
@@ -154,7 +153,18 @@
          ("C-j"      . ivy-next-line)
          ("C-k"      . ivy-previous-line)))
 
+(use-package centaur-tabs
+  :ensure t
+  :pin melpa
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward))
+
 (use-package swiper :ensure t :defer t
+  :pin melpa
   :init (ivy-mode)
   :config (setq ivy-use-virtual-buffers t
                 enable-recursive-minibuffers t)
@@ -193,19 +203,12 @@
   :config (counsel-projectile-mode) :init
   (setq projectile-switch-project-action 'projectile-dired))
 
-(use-package org-projectile
-  :pin melpa
-  :bind (("C-c n p" . org-projectile-project-todo-completing-read)
-         ("C-c c" . org-capture))
-  :config
-  (progn
-    (org-projectile-per-project)
-    (setq org-projectile-per-project-filepath ".notes/todo.org")
-    (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
-    (push (org-projectile-project-todo-entry) org-capture-templates))
-  :ensure t)
+;; org mode
 
-(use-package org-ql :pin melpa)
+
+(use-package annotate :ensure t :pin melpa)
+(use-package browse-at-remote :ensure t :pin melpa)
+(use-package cask :pin melpa :ensure t)
 
 ;; end projectile & ivy setup
 
@@ -219,7 +222,7 @@
 (use-package mood-one-theme :pin melpa)
 (use-package silkworm-theme :pin melpa)
 (use-package hydandata-light-theme :pin melpa)
-(use-package eziam-theme :pin melpa)
+;; (use-package eziam-theme :pin melpa)
 (use-package parchment-theme :pin melpa)
 (use-package nord-theme :pin melpa)
 ;; (use-package almost-mono-theme :pin melpa)
@@ -228,11 +231,16 @@
 ;; (load-theme 'brutalist-dark t)
 ;; (load-theme 'eziam-dark t)
 ;; (load-theme 'cloud t)
+;; (load-theme 'zerodark t)
 ;; (load-theme 'mood-one t)
 ;; (load-theme 'leuven t)
+
 ;; (load-theme 'gruvbox-dark-soft t)
 ;; (load-theme 'adwaita t)
-(load-theme 'zerodark t)
+;; (load-theme 'zerodark t)
+;; (load-theme 'poet-dark-monochrome t)
+(load-theme 'github-modern t)
+;; (load-theme 'hydandata-light t)
 ;; (load-theme 'faff)
 ;; (load-theme 'silkworm t)
 (zerodark-setup-modeline-format)
@@ -296,6 +304,8 @@
 
 (use-package flycheck :pin melpa :config (global-flycheck-mode))
 
+(use-package groovy-mode :pin melpa :ensure t)
+
 (use-package epg
   :config (setq epg-gpgconf-program "gpg"))
 
@@ -346,7 +356,7 @@
 ;; fish-shell supports
 (use-package fish-mode :pin melpa)
 
-(use-package rust-playground :pin melpa )
+(use-package rust-playground :pin melpa :ensure t)
 (use-package cargo :pin melpa)
 
 ;; jsonnet
@@ -358,12 +368,20 @@
 
 ;; ocaml supports
 (use-package tuareg :pin melpa)
+(use-package dune :pin melpa)
 
 ;; elixir supports
 (use-package elixir-mode :pin melpa)
 ;; (use-package elixir-mix :pin melpa)
 (use-package flycheck-elixir :pin melpa)
 (use-package flycheck-rust :pin melpa)
+
+(use-package flycheck-gradle :pin melpa
+  :ensure t
+  :commands (flycheck-gradle-setup)
+  :init (mapc (lambda (x)
+                (add-hook x #'flycheck-gradle-setup))
+              '(java-mode-hook kotlin-mode-hook)))
 
 ;; scala supports
 (use-package scala-mode :pin melpa :mode "\\.s\\(c\\|cala\\|bt\\)$")
@@ -374,33 +392,25 @@
 
 (global-auto-revert-mode t)
 
-;; (use-package auto-complete :pin melpa
-;;   :config
-;;   (ac-config-default)
-;;   (global-autocomplete-mode t))
-
-;; init lsp
 (use-package yasnippet :pin melpa)
-
-;; (use-package company-lsp :demand t :pin melpa)
-;; (use-package lsp-treemacs :demand t :pin melpa)
-;; elixir supports
-;; (use-package lsp-elixir :pin melpa :mode "\\.exs$")
-;; (use-package lsp-mode :pin melpa
-;;   :hook
-;;   (scala-mode . lsp)
-;;   (rust-mode . lsp)
-;;   (tuareg-mode . lsp)
-;;   (go-mode . lsp)
-;;   :commands lsp)
-;; (use-package lsp-ui :pin melpa :commands lsp-ui-mode)
-;; (use-package company-lsp :pin melpa :commands company-lsp)
-;; (use-package lsp-treemacs :pin melpa :commands lsp-treemacs-errors-list)
 
 ;; (use-package dap-mode :pin melpa)
 
 ;; abandon lsp-mode, let's try eglot
-(use-package eglot :pin melpa)
+(use-package eglot :pin melpa
+  :config (add-to-list 'eglot-server-programs '((caml-mode tuareg-mode) . ("ocamllsp"))))
+
+;; follow https://github.com/ubolonton/emacs-tree-sitter
+(use-package tree-sitter :pin ublt :ensure t
+  :config
+  (add-hook 'rust-mode-hook #'tree-sitter-mode)
+  (add-hook 'clojure-mode-hook #'tree-sitter-mode)
+  (add-hook 'java-mode-hook #'tree-sitter-mode)
+  (add-hook 'ocaml-mode-hook #'tree-sitter-mode)
+  (add-hook 'tuareg-mode-hook #'tree-sitter-mode))
+
+
+(use-package gif-screencast :pin melpa :ensure t)
 
 (use-package pass :pin melpa)
 (use-package password-store :pin melpa)
@@ -408,81 +418,159 @@
 (use-package auth-source-pass :pin melpa
   :init (auth-source-pass-enable))
 
-(use-package realgud :pin melpa)
-(use-package realgud-byebug :pin melpa)
-(use-package realgud-ipdb :pin melpa)
-(use-package realgud-lldb :pin melpa)
-(use-package realgud-node-debug :pin melpa)
-(use-package realgud-node-inspect :pin melpa)
-(use-package realgud-pry :pin melpa)
+;; (use-package realgud :pin melpa)
+;; (use-package realgud-byebug :pin melpa)
+;; (use-package realgud-ipdb :pin melpa)
+;; (use-package realgud-lldb :pin melpa)
+;; (use-package realgud-node-debug :pin melpa)
+;; (use-package realgud-node-inspect :pin melpa)
+;; (use-package realgud-pry :pin melpa)
+
 (use-package gradle-mode :pin melpa)
 (use-package kotlin-mode :pin melpa)
 
-(use-package ob-ammonite :pin melpa)
-(use-package ob-rust :quelpa (ob-rust :fetcher gitlab "ajyoon/ob-rust"))
-(use-package ob-go :quelpa (ob-go :fetcher gitlab "pope/ob-go"))
-(use-package ob-restclient :pin melpa)
+(use-package webfeeder :quelpa (webfeeder :fetcher gitlab :repo "ambrevar/emacs-webfeeder"))
+
 
 ; (use-package irfc :quelpa (irfc :fetcher url "https://www.emacswiki.org/emacs/download/irfc.el"))
 
 (quelpa '(irfc :url "https://www.emacswiki.org/emacs/download/irfc.el" :fetcher url))
 
-(use-package twittering-mode
-  :pin melpa
-  :ensure t
-  :commands twit
-  :init
-  (add-hook 'twittering-edit-mode-hook (lambda () (flyspell-mode)))
-  :config
-  (setq twittering-use-master-password t
-        twittering-private-info-file (expand-file-name "secrets" twitter-dir)
-        twittering-icon-mode t
-        twittering-use-icon-storage t
-        twittering-icon-storage-file (expand-file-name "twittering-mode-icons.gz" caches-dir)
-        twittering-convert-fix-size 52
-        twittering-initial-timeline-spec-string '(":home")
-        twittering-edit-skeleton 'inherit-any
-        twittering-display-remaining t
-        twittering-timeline-header  ""
-        twittering-timeline-footer  ""
-        twittering-status-format
-        "%i  %S, %RT{%FACE[bold]{%S}} %@  %FACE[shadow]{%p%f%L%r}\n%FOLD[        ]{%T}\n"))
-
-
 (use-package tla-pcal-mode :quelpa (tla-pcal-mode :fetcher github :repo "mrc/tla-tools"))
 (use-package proof-general :pin melpa :ensure t)
 (use-package wat-mode :quelpa (wat-mode :fetcher github :repo "devonsparks/wat-mode"))
+(use-package plantuml-mode :ensure t :pin melpa)
+(use-package flycheck-plantuml :ensure t :pin melpa)
 
-(setq org-src-tab-acts-natively t)
-(setq org-support-shift-select 'always)
+(use-package org :pin org
+  :ensure org-plus-contrib
+  :mode (("\\.org$" . org-mode))
+  :hook ((org-shiftleft-final . windmove-left)
+         (org-shiftdown-final . windmove-down)
+         (org-shiftright-final . windmove-right))
+  :init
+  (add-to-list 'org-src-lang-modes
+               '(("plantuml" . plantuml)
+                 ("scala" . ammonite)))
+  :config
+  (setq org-src-tab-acts-natively t
+        org-support-shift-select 'always
+        org-ditaa-jar-path "/usr/share/java/ditaa/ditaa-0.11.jar"
+        org-plantuml-jar-path "/usr/share/java/plantuml/plantuml.jar"))
 
+(use-package org-projectile
+  :pin melpa
+  :bind (("C-c n p" . org-projectile-project-todo-completing-read)
+         ("C-c c" . org-capture))
+  :config
+  (progn
+    (org-projectile-per-project)
+    (setq org-projectile-per-project-filepath ".notes/todo.org")
+    (setq org-agenda-files (append org-agenda-files (org-projectile-todo-files)))
+    (push (org-projectile-project-todo-entry) org-capture-templates))
+  :ensure t)
+
+(use-package ob-ammonite :pin melpa
+  :after org
+  :init
+  (add-to-list 'org-babel-load-languages '((ammonite . t))))
+
+(use-package ob-rust :quelpa (ob-rust :fetcher gitlab :repo "ajyoon/ob-rust")
+  :after org
+  :init
+  (add-to-list 'org-babel-load-languages '((rust . t))))
+
+(use-package ob-go :quelpa (ob-go :fetcher gitlab :repo "pope/ob-go")
+  :after org
+  :init
+  (add-to-list 'org-babel-load-languages '((go . t))))
+
+(use-package ob-restclient :pin melpa
+  :after org
+  :init
+  (add-to-list 'org-babel-load-languages '((restclient . t))))
+
+(use-package ob-mermaid :pin melpa :ensure t
+  :after org
+  :init
+  (add-to-list 'org-babel-load-languages '((mermaid . t))))
+
+(use-package ob-prolog :ensure t :pin melpa
+  :after org
+  :init
+  (add-to-list 'org-babel-load-languages '((prolog . t))))
+
+(use-package org-ql :pin melpa :ensure t)
+(use-package org-jira :pin melpa :ensure t)
 (use-package ox-rfc :pin melpa :ensure t)
 (use-package ox-epub :pin melpa :ensure t)
 (use-package ox-gfm :pin melpa :ensure t)
 (use-package ox-rst :pin melpa :ensure t)
-
+;; (use-package ob-diagram :pin melpa :ensure t)
 (use-package org-index :pin melpa :ensure t)
+;; (use-package org-ioslide :pin melpa :ensure t)
+(use-package org-kanban :pin melpa :ensure t)
+(use-package org-trello :pin melpa :ensure t)
+(use-package ox-ioslide :pin melpa :ensure t)
+(use-package org-tree-slide :pin melpa :ensure t)
+(use-package verb :pin melpa :ensure t)
 
+(use-package org-bullets
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
 
-(add-hook 'org-shiftleft-final-hook 'windmove-left)
-(add-hook 'org-shiftdown-final-hook 'windmove-down)
-(add-hook 'org-shiftright-final-hook 'windmove-right)
+(let* ((variable-tuple (cond ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
+                             ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
+                             ((x-list-fonts "Verdana")         '(:font "Verdana"))
+                             ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
+                             (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
+       (base-font-color     (face-foreground 'default nil 'default))
+       (headline           `(:inherit default :weight bold :foreground ,base-font-color)))
+  (custom-theme-set-faces 'user
+                          `(org-level-8 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-7 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-6 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-5 ((t (,@headline ,@variable-tuple))))
+                          `(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
+                          `(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
+                          `(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
+                          `(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
+                          `(org-document-title ((t (,@headline ,@variable-tuple :height 1.5 :underline nil))))))
+
+(use-package gitlab-ci-mode :pin melpa :ensure t)
+(use-package gitlab-ci-mode-flycheck :pin melpa :ensure t)
+
+(use-package orgit :ensure t :pin melpa)
 
 ;; (setq org-export-latex-listings 'minted)
 ;; (add-to-list 'org-export-latex-packages-alist '("" "minted"))
 
-(org-babel-do-load-languages
- 'org-babel-load-languages
- '(
-   (C . t)
-   (latex . t)
-   (css . t)
-   (shell . t)
-   (emacs-lisp . t)
-   (go . t)
-   (python . t)
-   (ocaml . t)
-   (rust . t)
-   (ammonite . t)))
+(defun org-blog-prepare (project-plist)
+  "With help from `https://github.com/howardabrams/dot-files'.
+  Touch `index.org' to rebuilt it.
+  Argument `PROJECT-PLIST' contains information about the current project."
+  (let* ((base-directory (plist-get project-plist :base-directory))
+         (buffer (find-file-noselect (expand-file-name "index.org" base-directory) t)))
+    (with-current-buffer buffer
+      (set-buffer-modified-p t)
+      (save-buffer 0))
+    (kill-buffer buffer)))
 
-;; no provide since it's init.el
+(defun org-blog-sitemap-format-entry (entry _style project)
+  "Return string for each ENTRY in PROJECT."
+  (when (s-starts-with-p "posts/" entry)
+    (format "@@html:<span class=\"archive-item\"><span class=\"archive-date\">@@ %s @@html:</span>@@ [[file:%s][%s]] @@html:</span>@@"
+            (format-time-string "%h %d, %Y" (org-publish-find-date entry project))
+            entry
+            (org-publish-find-title entry project))))
+
+
+(defun org-blog-sitemap-function (title list)
+  "Return sitemap using TITLE and LIST returned by `org-blog-sitemap-format-entry'."
+  (concat "#+TITLE: " title "\n\n"
+          "\n#+begin_archive\n"
+          (mapconcat (lambda (li)
+                       (format "@@html:<li>@@ %s @@html:</li>@@" (car li)))
+                     (seq-filter #'car (cdr list))
+                     "\n")
+          "\n#+end_archive\n"))
