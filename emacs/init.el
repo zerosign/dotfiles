6586c8b1d9827b92491ecc0b72c5bfc50e96177d
zerosign <r1nlx0@gmail.com>
2020-10-29 21:12:12 +0700

    add default application freedesktop *.desktop
    Signed-off-by: zerosign <r1nlx0@gmail.com>

--- emacs/init.el
+++ emacs/init.el
@@ -21,8 +21,8 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 
-(setq-default indent-tabs-mode nil)
-(setq-default tab-width 3)
+(setq-default indent-tabs-mode nil
+              tab-width 3)
 (setq inhibit-startup-screen t
       initial-scratch-message nil
       initial-major-mode 'text-mode
@@ -69,7 +69,7 @@
 
 ;; (load-theme 'poet t)
 ;; (load-theme 'ayu-light t)
-(load-theme 'modus-operandi t)
+(load-theme 'leuven t)
 
 ;; (straight-use-package 'deadgrep)
 (use-package deadgrep :straight t :bind (("C-c C-s"  . deadgrep)))
@@ -101,38 +101,38 @@
 
 (add-hook 'prog-mode-hook 'company-mode)
 
-;; (straight-use-package 'prescient)
-(use-package prescient :straight t :defer t)
-;; (prescient-persist-mode +1)
+(use-package prescient :straight t :defer t
+  :config (prescient-persist-mode +1))
 
-;; (straight-use-package 'ivy-prescient)
-(use-package ivy-prescient :straight t :defer t)
 ;; (ivy-prescient-mode +1)
 
 ;; (straight-use-package 'company-prescient)
-(use-package company-prescient :straight t :defer t)
-;;(company-prescient-mode +1)
+(use-package company-prescient :straight t
+  :after company-mode
+  :defer t
+  :config (company-prescient-mode +1))
 
 (use-package protobuf-mode
   :init
   (add-to-list 'auto-mode-alist '("\\.proto$" . protobuf-mode)))
 
-;; (straight-use-package 'selectrum-prescient)
 ;; (use-package selectrum-prescient)
 
-;; (straight-use-package 'ivy)
-;; (straight-use-package 'smex)
-;; (straight-use-package 'flx)
+(use-package counsel :straight t :demand t
+  :bind (("M-x" . counsel-M-x)))
+
 (use-package ivy :straight t :demand t :diminish ivy-mode
   :init
-  (setq ivy-use-virtual-buffers t)
-  (setq ivy-initial-inputs-alist nil)
-  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
-  (setq ivy-extra-directories nil)
+  (setq ivy-use-virtual-buffers t
+        ivy-initial-inputs-alist nil
+        ivy-re-builders-alist '((t . ivy--regex-fuzzy))
+        ivy-extra-directories nil)
   :config
   (define-key ivy-minibuffer-map (kbd "C-l") (kbd "DEL"))
-  (use-package flx :straight t :ensure t)
+  (use-package flx :straight t :defer t)
+  (use-package ivy-prescient :straight t :defer t)
   (ivy-mode)
+  (ivy-prescient-mode)
   :bind (("C-c C-r" . ivy-resume)
          ("<f6>"    . ivy-resume)
          ("s-b"     . ivy-switch-buffer)
@@ -142,7 +142,7 @@
          ("C-j"      . ivy-next-line)
          ("C-k"      . ivy-previous-line)))
 
-(straight-use-package 'swiper)
+(use-package swiper :straight t)
 (global-set-key "\C-s" 'swiper)
 
 ;; (straight-use-package 'magit)
@@ -153,39 +153,46 @@
   :hook ((magit-popup-mode-hook . no-trailing-whitespace)
 	 (git-commit-mode . goto-address-mode)))
 
+(use-package exec-path-from-shell :straight t :defer t)
+
+(when (daemonp)
+  (exec-path-from-shell-initialize))
+
 ;; (straight-use-package 'magit-todos)
 (use-package magit-todos :straight t :defer t)
 
 (use-package magit-delta :straight t :defer t)
 
 (require 'subr-x)
-(straight-use-package 'git)
+(use-package git :straight t)
 
-(defun org-git-version ()
-  "The Git version of org-mode.
-Inserted by installing org-mode or when a release is made."
-  (require 'git)
-  (let ((git-repo (expand-file-name
-                   "straight/repos/org/" user-emacs-directory)))
-    (string-trim
-     (git-run "describe"
-              "--match=release\*"
-              "--abbrev=6"
-              "HEAD"))))
+(straight-use-package '(org-plus-contrib :includes (org)))
 
-(defun org-release ()
-  "The release version of org-mode.
-Inserted by installing org-mode or when a release is made."
-  (require 'git)
-  (let ((git-repo (expand-file-name
-                   "straight/repos/org/" user-emacs-directory)))
-    (string-trim
-     (string-remove-prefix
-      "release_"
-      (git-run "describe"
-               "--match=release\*"
-               "--abbrev=0"
-               "HEAD")))))
+;; (defun org-git-version ()
+;;   "The Git version of org-mode.
+;; Inserted by installing org-mode or when a release is made."
+;;   (require 'git)
+;;   (let ((git-repo (expand-file-name
+;;                    "straight/repos/org/" user-emacs-directory)))
+;;     (string-trim
+;;      (git-run "describe"
+;;               "--match=release\*"
+;;               "--abbrev=6"
+;;               "HEAD"))))
+;;
+;; (defun org-release ()
+;;   "The release version of org-mode.
+;; Inserted by installing org-mode or when a release is made."
+;;   (require 'git)
+;;   (let ((git-repo (expand-file-name
+;;                    "straight/repos/org/" user-emacs-directory)))
+;;     (string-trim
+;;      (string-remove-prefix
+;;       "release_"
+;;       (git-run "describe"
+;;                "--match=release\*"
+;;                "--abbrev=0"
+;;                "HEAD")))))
 
 (use-package plantuml-mode :straight t
   :config
@@ -195,45 +202,45 @@ Inserted by installing org-mode or when a release is made."
 (use-package flycheck-plantuml :straight t
   :hook (plantuml . flycheck))
 
-(use-package org-plus-contrib :straight t
-  :mode (("\\.org$" . org-mode))
-  :init
-  (require 'ob-dot)
-  (require 'ob-plantuml)
-  (setq org-plantuml-jar-path "/usr/share/java/plantuml/plantuml.jar")
-  :config
-  (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot))
-  (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((clojure . t)
-     (emacs-lisp . t)
-     (gnuplot . t)
-     (java . t)
-     (clojure . t)
-     (dot . t)
-     (C . t)
-     (lilypond . t)
-     (ruby . t)
-     ;; (rust . t)
-     ;; (rustic . t)
-     (python . t)
-     (dot . t)
-     (screen . t)
-     (fortran . t)
-     ;; (prolog . t)
-     ;; (kotlin . t)
-     ;; (http . t)
-     ;; (R . t)
-     (ditaa . t)
-     (js . t)
-     (ammonite . t)
-     (shell . t)
-     (sql . t)
-     (plantuml . t)
-     (ocaml . t)))
-  :bind (("C-c l" . org-store-link)
-         ("C-c a" . org-agenda)))
+;; (use-package org-plus-contrib :straight t
+;;   :mode (("\\.org$" . org-mode))
+;;   :init
+;;   (require 'ob-dot)
+;;   (require 'ob-plantuml)
+;;   (setq org-plantuml-jar-path "/usr/share/java/plantuml/plantuml.jar")
+;;   :config
+;;   (add-to-list 'org-src-lang-modes '("dot" . graphviz-dot))
+;;   (add-to-list 'org-src-lang-modes '("plantuml" . plantuml))
+;;   (org-babel-do-load-languages
+;;    'org-babel-load-languages
+;;    '((clojure . t)
+;;      (emacs-lisp . t)
+;;      (gnuplot . t)
+;;      (java . t)
+;;      (clojure . t)
+;;      (dot . t)
+;;      (C . t)
+;;      (lilypond . t)
+;;      (ruby . t)
+;;      ;; (rust . t)
+;;      ;; (rustic . t)
+;;      (python . t)
+;;      (dot . t)
+;;      (screen . t)
+;;      (fortran . t)
+;;      ;; (prolog . t)
+;;      ;; (kotlin . t)
+;;      ;; (http . t)
+;;      ;; (R . t)
+;;      (ditaa . t)
+;;      (js . t)
+;;      (ammonite . t)
+;;      (shell . t)
+;;      (sql . t)
+;;      (plantuml . t)
+;;      (ocaml . t)))
+;;   :bind (("C-c l" . org-store-link)
+;;          ("C-c a" . org-agenda)))
 
 (use-package org-projectile
   :straight t
@@ -252,7 +259,31 @@ Inserted by installing org-mode or when a release is made."
 ;; ;;   ("C-c l" . org-store-link)
 ;; ;;   ("C-c a" . org-agenda))
 
+(use-package lsp-metals :straight t)
+(use-package lsp-java :straight t)
 
+(use-package lsp-mode
+  :straight t
+  :init
+  (setq lsp-rust-server 'rust-analyzer
+        read-process-output-max (* 1024 1024)
+        lsp-idle-delay 0.500
+        lsp-enable-file-watchers nil
+        gc-cons-threshold 100000000)
+  :hook ((crystal-mode . lsp)
+         (tuareg-mode . lsp)
+         (kotlin-mode . lsp)
+         (typescript-mode . lsp)
+         (rust-mode . lsp)
+         (go-mode . lsp)
+         (scala-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui :straight t :commands lsp-ui-mode)
+
+;; if you are ivy user
+(use-package lsp-ivy :straight t :commands lsp-ivy-workspace-symbol)
 ;; ;; (org-babel-do-load-languages
 ;; ;;  'org-babel-load-languages
 ;; ;;  '((clojure . t)
@@ -284,7 +315,7 @@ Inserted by installing org-mode or when a release is made."
 
 (use-package polymode :straight t)
 (use-package poly-markdown :straight t)
-;; (use-package poly-org :straight t)
+(use-package poly-org :straight t)
 
 (add-to-list 'auto-mode-alist '("\\.md" . poly-markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.org" . poly-org-mode))
@@ -297,9 +328,8 @@ Inserted by installing org-mode or when a release is made."
 (add-hook 'org-shiftdown-final-hook 'windmove-down)
 (add-hook 'org-shiftright-final-hook 'windmove-right)
 
-(straight-use-package 'forge)
+(use-package forge :straight t)
 
-(straight-use-package 'projectile)
 (use-package projectile :straight t :diminish projectile-mode
   :init
   (setq projectile-enable-caching t
@@ -310,6 +340,11 @@ Inserted by installing org-mode or when a release is made."
   :config (projectile-mode)
   :bind (("C-c f" . projectile-find-file)
 	 ("C-c p" . projectile-switch-project)))
+
+(straight-register-package
+ '(tsc :host github
+       :repo "ubolonton/emacs-tree-sitter"
+       :files ("core/*.el")))
 
 (straight-use-package
  '(tree-sitter :host github
@@ -333,54 +368,28 @@ Inserted by installing org-mode or when a release is made."
 
 (global-tree-sitter-mode)
 
-(straight-use-package 'go-mode)
-(straight-use-package 'rust-mode)
-(straight-use-package 'scala-mode)
-(straight-use-package 'tuareg)
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+
+(use-package go-mode :straight t)
+
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+(use-package rust-mode :straight t)
+(use-package scala-mode :straight t)
+(use-package tuareg :straight t)
 
 (use-package flycheck-mercury :straight t)
-
-(straight-use-package 'verb)
+(use-package verb :straight t)
 
 (use-package racket-mode :straight t)
 
 (use-package crystal-mode :straight t)
 
-(use-package company-lsp :straight t)
-
-(use-package lsp-metals :straight t)
-
-(use-package lsp-mode
-  :straight t
-  :init
-  (setq lsp-rust-server 'rust-analyzer)
-  :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
-         ;; if you want which-key integration
-         ;; (racket-mode . lsp-racket-enable)
-         (crystal-mode . lsp)
-         (tuareg-mode . lsp)
-         (kotlin-mode . lsp)
-         (typescript-mode . lsp)
-         (rust-mode . lsp)
-         (go-mode . lsp)
-         (scala-mode . lsp)
-         (lsp-mode . lsp-enable-which-key-integration))
-  :commands lsp)
-
-;; optionally
-(use-package lsp-ui :straight t :commands lsp-ui-mode)
-;; if you are helm user
-(use-package helm-lsp :straight t :commands helm-lsp-workspace-symbol)
-;; if you are ivy user
-(use-package lsp-ivy :straight t :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :pin melpa :commands lsp-treemacs-errors-list)
-
 (use-package rmsbolt :straight t)
 
 ;; optionally if you want to use debugger
-(use-package dap-mode :straight t)
-;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
 (use-package treemacs :straight t)
 
 ;; optional if you want which-key integration
@@ -389,7 +398,8 @@ Inserted by installing org-mode or when a release is made."
   :config
   (which-key-mode))
 
-(use-package flycheck :straight t)
+(use-package flycheck :straight t
+  :config (global-flycheck-mode))
 
 (use-package epg :straight t :config (setq epg-gpgconf-program "gpg"))
 
@@ -406,7 +416,7 @@ Inserted by installing org-mode or when a release is made."
 (use-package pass :straight t)
 (use-package password-store :straight t)
 (use-package password-store-otp :straight t)
-(use-package auth-source-pass :straight t :init (auth-source-pass-enable))
+(use-package auth-source-pass :straight t :config (auth-source-pass-enable))
 
 (use-package graphviz-dot-mode :straight t)
 
@@ -443,14 +453,13 @@ Inserted by installing org-mode or when a release is made."
 ;; (setq interprogram-cut-function 'wl-copy)
 ;; (setq interprogram-paste-function 'wl-paste)
 
-(use-package gist :straight t
-  :config
-  (setq gist-list-format '((created "Created" 15 nil "%D" "%R")
-                           (id      "Id"      10 nil identity)
-                           (files   "Files"   35 nil (lambda (files) (mapconcat 'identity files ", ")))
-                           (visibility "Private" 8 nil (lambda (public) (or (and public "") "   Y")))
-                           (description "Description" 0 nil identity))))
-
+;; (use-package gist :straight t
+;;   :config
+;;   (setq gist-list-format '((created "Created" 15 nil "%D" "%R")
+;;                            (id      "Id"      10 nil identity)
+;;                            (files   "Files"   35 nil (lambda (files) (mapconcat 'identity files ", ")))
+;;                            (visibility "Private" 8 nil (lambda (public) (or (and public "") "   Y")))
+;;                            (description "Description" 0 nil identity))))
 
 (use-package browse-at-remote :straight t)
 
@@ -470,3 +479,42 @@ Inserted by installing org-mode or when a release is made."
 
 (add-to-list 'auto-mode-alist '("\\.spthy" . spthy-mode))
 (add-to-list 'auto-mode-alist '("\\.tla" . tla+-mode))
+
+(global-set-key (kbd "C-c C-_") 'comment-or-uncomment-region)
+
+(use-package json-mode :straight t)
+
+(use-package jdecomp :straight t
+  :config
+  (setq
+   jdecomp-decompiler-paths '((fernflower . "/home/zerosign/.m2/repository/org/jetbrains/fernflower/fernflower/1.0/fernflower-1.0.jar"))
+   jdecomp-decompiler-type 'fernflower))
+
+
+(use-package editorconfig :straight t :config (editorconfig-mode 1))
+
+(use-package deft :straight t
+  :custom
+  (deft-extensions '("org" "md" "txt"))
+  (deft-directory  "~/Repositories/notes/zerodevs")
+  (deft-use-filename-as-title t))
+
+(use-package zetteldeft :straight t :after deft config (zettledeft-set-classic-keybindings))
+
+(use-package julia-mode :straight t)
+
+(use-package hcl-mode :straight t)
+
+(use-package meson-mode :straight t)
+
+(unless (display-graphic-p)
+  (defun scroll-mouse-up ()
+    (interactive)
+    (scroll-up 15))
+  (defun scroll-mouse-down ()
+    (interactive)
+    (scroll-down 15))
+  ;; activate mouse-based scrolling
+  (xterm-mouse-mode 1)
+  (global-set-key (kbd "<mouse-4>") 'scroll-mouse-down)
+  (global-set-key (kbd "<mouse-5>") 'scroll-mouse-up))
